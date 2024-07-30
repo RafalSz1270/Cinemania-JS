dfc3b9bc38ffdd03eaa9e38558f0241900de2942
MaciejBratko <maciej.bratko@outlook.com>
2024-07-30 23:25:28 +0200

    użyłem fetchMoviesData() z home.js do wypełnienia modala

--- src/modalOpen.js
+++ src/modalOpen.js
@@ -1,13 +1,35 @@
-export const initializeModal = () => {
+import { fetchMoviesData } from './home.js';
+
+export const initializeModal = async () => {
   const modal = document.getElementById('myModal');
-  const btn = document.getElementById('myBtn');
   const span = document.getElementsByClassName('close')[0];
   const toggleButton = document.getElementById('toggleLibrary');
+  const movieContainer = document.querySelector('#movie-container');
 
-  // Open the modal
-  btn.onclick = function () {
+  // Fetch movie data
+  const movies = await fetchMoviesData();
+
+  // Add click event listeners to movie posters
+  movieContainer.addEventListener('click', event => {
+    const moviePoster = event.target.closest('.movie-head-poster');
+    if (moviePoster) {
+      const index = Array.from(movieContainer.children).indexOf(moviePoster);
+      openModal(movies[index]);
+    }
+  });
+
+  function openModal(movie) {
+    const modalContent = document.querySelector('.modal-body');
+    modalContent.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} Poster">
+      <h2>${movie.title}</h2>
+      <p><strong>Vote / Votes</strong> ${movie.vote_average}/${movie.vote_count}</p>
+      <p><strong>Popularity</strong> ${movie.popularity}</p>
+      <p><strong>Genre</strong> ${movie.genre_names}</p>
+      <p><strong>About</strong> ${movie.overview}</p>
+    `;
     modal.style.display = 'block';
-  };
+  }
 
   // Close the modal
   span.onclick = function () {
